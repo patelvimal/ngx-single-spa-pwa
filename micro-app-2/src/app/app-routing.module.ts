a51d417d26ab4a415313b38e733a68cecfbbb1da
Vimal Patel <vimal.patel.nvs@gmail.com>
2021-06-13 19:03:32 +0530

    Child route added  in App2 side nav bar updated

--- micro-app-2/src/app/app-routing.module.ts
+++ micro-app-2/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { CommonModule } from '@angular/common';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [ 
-  { path: 'product', component: ProductComponent},
-  { path: '', redirectTo: '/product', pathMatch: 'full' },
+  { path: 'product', 
+    component: ProductComponent,
+    children: [{ path: 'detail', component: ProductDetailComponent }],
+  },
+  { path: '', 
+    redirectTo: '/product', 
+    pathMatch: 'full' 
+  },
 ];
 
 @NgModule({
--- root-app/src/app/header/header.component.ts
+++ root-app/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  showFiller = false;
-  constructor() { }
+  opened = true;
+  @ViewChild('sidenav') sidenav: MatSidenav | null = null;
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    if (event.target.innerWidth < 768) {
+      this.sidenav.fixedTopGap = 55;
+      this.opened = false;
+    } else {
+      this.sidenav.fixedTopGap = 55
+      this.opened = true;
+    }
+  }
+
+  isBiggerScreen() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (width < 768) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
