4639e9bfbaf12284ddf92357a8086fa33ee593ad
Vimal Patel <vimal.patel.nvs@gmail.com>
2021-06-13 09:01:57 +0530

    updated route to empty to troubleshoot the issue

--- micro-app-2/src/app/app-routing.module.ts
+++ micro-app-2/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [ { path: '', redirectTo: '/product', pathMatch: 'full' },
+const routes: Routes = [ 
+{ path: '', redirectTo: '/product', pathMatch: 'full' },
 {
   path: 'product',
   component: ProductComponent,
   children: [{ path: 'detail', component: ProductDetailComponent }],
-},];
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
